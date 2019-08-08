48bc27d6c401e0b8977ac8f1e2c6697af3abe92d
mppc76 <35976720+mppc76@users.noreply.github.com>
2019-08-08 16:34:35 -0500

    se eliminan carpetas no necesarias

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,12 +7,6 @@
 
 import { HTMLStencilElement, JSXBase } from '@stencil/core/internal';
 import {
-  StkButtonType,
-} from './components/stk-components/general/button/stk-button';
-import {
-  StkSpan,
-} from './components/stk-components/grid/stk-col/stk-col';
-import {
   inputType,
 } from './components/stk-components/data-entry/input/stk-input';
 import {
@@ -35,37 +29,6 @@ export namespace Components {
     */
     'middle': string;
   }
-  interface StkBreadcrumb {}
-  interface StkBreadcrumbItem {
-    'label': string;
-  }
-  interface StkButton {
-    'block': boolean;
-    'disabled': boolean;
-    'icon': string;
-    'loading': boolean;
-    'type': StkButtonType;
-  }
-  interface StkButtonGroup {}
-  interface StkCol {
-    'lg': StkSpan;
-    'lgOffset': StkSpan;
-    'md': StkSpan;
-    'mdOffset': StkSpan;
-    'offset': StkSpan;
-    'sm': StkSpan;
-    'smOffset': StkSpan;
-    'span': StkSpan;
-    'xl': StkSpan;
-    'xlOffset': StkSpan;
-    'xs': StkSpan;
-    'xsOffset': StkSpan;
-  }
-  interface StkContent {}
-  interface StkFooter {}
-  interface StkHeader {}
-  interface StkHeaderLogo {}
-  interface StkHeaderMenu {}
   interface StkInput {
     'allowClear': boolean;
     'defaultValue': string;
@@ -129,15 +92,6 @@ export namespace Components {
     'validator': Array<string | ValidatorEntry | Validator<string>>;
     'value': string;
   }
-  interface StkLayout {
-    'innerLayout': boolean;
-  }
-  interface StkMenuItem {
-    'label': string;
-  }
-  interface StkRow {}
-  interface StkSider {}
-  interface StkSiderMenu {}
 }
 
 declare global {
@@ -147,66 +101,6 @@ declare global {
   var HTMLMyComponentElement: {
     prototype: HTMLMyComponentElement;
     new (): HTMLMyComponentElement;
-  };
-
-  interface HTMLStkBreadcrumbElement extends Components.StkBreadcrumb, HTMLStencilElement {}
-  var HTMLStkBreadcrumbElement: {
-    prototype: HTMLStkBreadcrumbElement;
-    new (): HTMLStkBreadcrumbElement;
-  };
-
-  interface HTMLStkBreadcrumbItemElement extends Components.StkBreadcrumbItem, HTMLStencilElement {}
-  var HTMLStkBreadcrumbItemElement: {
-    prototype: HTMLStkBreadcrumbItemElement;
-    new (): HTMLStkBreadcrumbItemElement;
-  };
-
-  interface HTMLStkButtonElement extends Components.StkButton, HTMLStencilElement {}
-  var HTMLStkButtonElement: {
-    prototype: HTMLStkButtonElement;
-    new (): HTMLStkButtonElement;
-  };
-
-  interface HTMLStkButtonGroupElement extends Components.StkButtonGroup, HTMLStencilElement {}
-  var HTMLStkButtonGroupElement: {
-    prototype: HTMLStkButtonGroupElement;
-    new (): HTMLStkButtonGroupElement;
-  };
-
-  interface HTMLStkColElement extends Components.StkCol, HTMLStencilElement {}
-  var HTMLStkColElement: {
-    prototype: HTMLStkColElement;
-    new (): HTMLStkColElement;
-  };
-
-  interface HTMLStkContentElement extends Components.StkContent, HTMLStencilElement {}
-  var HTMLStkContentElement: {
-    prototype: HTMLStkContentElement;
-    new (): HTMLStkContentElement;
-  };
-
-  interface HTMLStkFooterElement extends Components.StkFooter, HTMLStencilElement {}
-  var HTMLStkFooterElement: {
-    prototype: HTMLStkFooterElement;
-    new (): HTMLStkFooterElement;
-  };
-
-  interface HTMLStkHeaderElement extends Components.StkHeader, HTMLStencilElement {}
-  var HTMLStkHeaderElement: {
-    prototype: HTMLStkHeaderElement;
-    new (): HTMLStkHeaderElement;
-  };
-
-  interface HTMLStkHeaderLogoElement extends Components.StkHeaderLogo, HTMLStencilElement {}
-  var HTMLStkHeaderLogoElement: {
-    prototype: HTMLStkHeaderLogoElement;
-    new (): HTMLStkHeaderLogoElement;
-  };
-
-  interface HTMLStkHeaderMenuElement extends Components.StkHeaderMenu, HTMLStencilElement {}
-  var HTMLStkHeaderMenuElement: {
-    prototype: HTMLStkHeaderMenuElement;
-    new (): HTMLStkHeaderMenuElement;
   };
 
   interface HTMLStkInputElement extends Components.StkInput, HTMLStencilElement {}
@@ -244,59 +138,14 @@ declare global {
     prototype: HTMLStkInputTextElement;
     new (): HTMLStkInputTextElement;
   };
-
-  interface HTMLStkLayoutElement extends Components.StkLayout, HTMLStencilElement {}
-  var HTMLStkLayoutElement: {
-    prototype: HTMLStkLayoutElement;
-    new (): HTMLStkLayoutElement;
-  };
-
-  interface HTMLStkMenuItemElement extends Components.StkMenuItem, HTMLStencilElement {}
-  var HTMLStkMenuItemElement: {
-    prototype: HTMLStkMenuItemElement;
-    new (): HTMLStkMenuItemElement;
-  };
-
-  interface HTMLStkRowElement extends Components.StkRow, HTMLStencilElement {}
-  var HTMLStkRowElement: {
-    prototype: HTMLStkRowElement;
-    new (): HTMLStkRowElement;
-  };
-
-  interface HTMLStkSiderElement extends Components.StkSider, HTMLStencilElement {}
-  var HTMLStkSiderElement: {
-    prototype: HTMLStkSiderElement;
-    new (): HTMLStkSiderElement;
-  };
-
-  interface HTMLStkSiderMenuElement extends Components.StkSiderMenu, HTMLStencilElement {}
-  var HTMLStkSiderMenuElement: {
-    prototype: HTMLStkSiderMenuElement;
-    new (): HTMLStkSiderMenuElement;
-  };
   interface HTMLElementTagNameMap {
     'my-component': HTMLMyComponentElement;
-    'stk-breadcrumb': HTMLStkBreadcrumbElement;
-    'stk-breadcrumb-item': HTMLStkBreadcrumbItemElement;
-    'stk-button': HTMLStkButtonElement;
-    'stk-button-group': HTMLStkButtonGroupElement;
-    'stk-col': HTMLStkColElement;
-    'stk-content': HTMLStkContentElement;
-    'stk-footer': HTMLStkFooterElement;
-    'stk-header': HTMLStkHeaderElement;
-    'stk-header-logo': HTMLStkHeaderLogoElement;
-    'stk-header-menu': HTMLStkHeaderMenuElement;
     'stk-input': HTMLStkInputElement;
     'stk-input-currency': HTMLStkInputCurrencyElement;
     'stk-input-email': HTMLStkInputEmailElement;
     'stk-input-password': HTMLStkInputPasswordElement;
     'stk-input-phone': HTMLStkInputPhoneElement;
     'stk-input-text': HTMLStkInputTextElement;
-    'stk-layout': HTMLStkLayoutElement;
-    'stk-menu-item': HTMLStkMenuItemElement;
-    'stk-row': HTMLStkRowElement;
-    'stk-sider': HTMLStkSiderElement;
-    'stk-sider-menu': HTMLStkSiderMenuElement;
   }
 }
 
@@ -315,37 +164,6 @@ declare namespace LocalJSX {
     */
     'middle'?: string;
   }
-  interface StkBreadcrumb extends JSXBase.HTMLAttributes<HTMLStkBreadcrumbElement> {}
-  interface StkBreadcrumbItem extends JSXBase.HTMLAttributes<HTMLStkBreadcrumbItemElement> {
-    'label'?: string;
-  }
-  interface StkButton extends JSXBase.HTMLAttributes<HTMLStkButtonElement> {
-    'block'?: boolean;
-    'disabled'?: boolean;
-    'icon'?: string;
-    'loading'?: boolean;
-    'type'?: StkButtonType;
-  }
-  interface StkButtonGroup extends JSXBase.HTMLAttributes<HTMLStkButtonGroupElement> {}
-  interface StkCol extends JSXBase.HTMLAttributes<HTMLStkColElement> {
-    'lg'?: StkSpan;
-    'lgOffset'?: StkSpan;
-    'md'?: StkSpan;
-    'mdOffset'?: StkSpan;
-    'offset'?: StkSpan;
-    'sm'?: StkSpan;
-    'smOffset'?: StkSpan;
-    'span'?: StkSpan;
-    'xl'?: StkSpan;
-    'xlOffset'?: StkSpan;
-    'xs'?: StkSpan;
-    'xsOffset'?: StkSpan;
-  }
-  interface StkContent extends JSXBase.HTMLAttributes<HTMLStkContentElement> {}
-  interface StkFooter extends JSXBase.HTMLAttributes<HTMLStkFooterElement> {}
-  interface StkHeader extends JSXBase.HTMLAttributes<HTMLStkHeaderElement> {}
-  interface StkHeaderLogo extends JSXBase.HTMLAttributes<HTMLStkHeaderLogoElement> {}
-  interface StkHeaderMenu extends JSXBase.HTMLAttributes<HTMLStkHeaderMenuElement> {}
   interface StkInput extends JSXBase.HTMLAttributes<HTMLStkInputElement> {
     'allowClear'?: boolean;
     'defaultValue'?: string;
@@ -415,39 +233,15 @@ declare namespace LocalJSX {
     'validator'?: Array<string | ValidatorEntry | Validator<string>>;
     'value'?: string;
   }
-  interface StkLayout extends JSXBase.HTMLAttributes<HTMLStkLayoutElement> {
-    'innerLayout'?: boolean;
-  }
-  interface StkMenuItem extends JSXBase.HTMLAttributes<HTMLStkMenuItemElement> {
-    'label'?: string;
-  }
-  interface StkRow extends JSXBase.HTMLAttributes<HTMLStkRowElement> {}
-  interface StkSider extends JSXBase.HTMLAttributes<HTMLStkSiderElement> {}
-  interface StkSiderMenu extends JSXBase.HTMLAttributes<HTMLStkSiderMenuElement> {}
 
   interface IntrinsicElements {
     'my-component': MyComponent;
-    'stk-breadcrumb': StkBreadcrumb;
-    'stk-breadcrumb-item': StkBreadcrumbItem;
-    'stk-button': StkButton;
-    'stk-button-group': StkButtonGroup;
-    'stk-col': StkCol;
-    'stk-content': StkContent;
-    'stk-footer': StkFooter;
-    'stk-header': StkHeader;
-    'stk-header-logo': StkHeaderLogo;
-    'stk-header-menu': StkHeaderMenu;
     'stk-input': StkInput;
     'stk-input-currency': StkInputCurrency;
     'stk-input-email': StkInputEmail;
     'stk-input-password': StkInputPassword;
     'stk-input-phone': StkInputPhone;
     'stk-input-text': StkInputText;
-    'stk-layout': StkLayout;
-    'stk-menu-item': StkMenuItem;
-    'stk-row': StkRow;
-    'stk-sider': StkSider;
-    'stk-sider-menu': StkSiderMenu;
   }
 }
 
